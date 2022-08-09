e660ff5460a9d2c1194cfb3819d66f3a86432235
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-09 19:44:09 +0530

    Add files via upload

---
+++ SUBSTRACTION.java
@@ -0,0 +1,9 @@
+class SUBSTRACTION {
+public static void main(String [] args){
+System.out.println("The two number is:");
+int c = 45;
+int d = 29;
+System.out.println( c + " " + d );
+int substract = c-d ;
+System.out.println( " The substract is :" + substract);
+}}
