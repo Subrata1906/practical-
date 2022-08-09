82100a79cb069bee7cb481317dd03668f4037642
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-09 19:43:35 +0530

    Add files via upload

---
+++ ADDITION.java
@@ -0,0 +1,9 @@
+class ADDITION {
+public static void main(String [] args){
+System.out.println("The two number is:");
+int a = 5;
+int b = 9;
+System.out.println(a + " " + b );
+int sum = a+b ;
+System.out.println( " The sum is :" + sum);
+}}
