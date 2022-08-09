8a29f02d603319b4436a90e61c688104682fa581
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-09 19:44:57 +0530

    Add files via upload

---
+++ DIVITION.java
@@ -0,0 +1,9 @@
+class DIVITION {
+public static void main(String [] args){
+System.out.println("The two number is:");
+int p = 40;
+int q = 5;
+System.out.println( p + " " + q );
+int divition = p/q ;
+System.out.println( " The divition is :" + divition);
+}}
