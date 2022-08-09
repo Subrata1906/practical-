d89e09b886a2836344cf2f0f3e6a87e4b1c8fc2f
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-09 19:44:35 +0530

    Add files via upload

---
+++ MULTIPLICATION.java
@@ -0,0 +1,9 @@
+class MULTIPLICATION {
+public static void main(String [] args){
+System.out.println("The two number is:");
+int m = 15;
+int n = 5;
+System.out.println( m + " " + n );
+int multiply = m*n ;
+System.out.println( " The multipliction is :" + multiply);
+}}
