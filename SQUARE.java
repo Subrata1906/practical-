f5b573888161c588fa30db363577e9f76d9b417f
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-12 12:12:58 +0530

    Add files via upload

---
+++ SQUARE.java
@@ -0,0 +1,12 @@
+class SQUARE {
+public static void main(String [] args){
+System.out.println("One side of square is:");
+int a = 15;
+System.out.println( a );
+int areaofsquare = a*a ;
+System.out.println( " Area of the square is :" + areaofsquare);
+
+
+int peremeterofsquare = a*4 ;
+System.out.println( " The peremeter of square is :" + peremeterofsquare);
+}}
