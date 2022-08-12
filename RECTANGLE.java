990a3f20445260f9e96176bace9a89c31ec01f20
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-12 12:12:15 +0530

    Add files via upload

---
+++ RECTANGLE.java
@@ -0,0 +1,15 @@
+class RECTANGLE {
+public static void main(String [] args){
+System.out.println("Length of rectangle is:");
+int a = 20;
+System.out.println( a );
+System.out.println("Width of rectangle is:");
+int b = 15;
+System.out.println( b );
+int areaofrectangle = a*b ;
+System.out.println( " Area of the rectangle is :" + areaofrectangle);
+
+
+int peremeterofrectangle = 2*(a+b) ;
+System.out.println( " The peremeter of rectangle is :" + peremeterofrectangle);
+}}
