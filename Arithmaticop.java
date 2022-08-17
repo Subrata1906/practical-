86a6e7a2a98f26e07644f75a594dea0c8396f3c5
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-17 17:32:49 +0530

    Add files via upload

---
+++ Arithmaticop.java
@@ -0,0 +1,25 @@
+package Operator1;
+
+public class Arithmaticop {
+
+	public static void main(String[] args) {
+		int a = 20;
+	    int b = 30;
+		   System.out.println(a+b);
+		   System.out.println(a-b);
+		   System.out.println(a*b);
+		   System.out.println(a/b);
+		   System.out.println(a%b);
+		   
+		   System.out.println(a<b&&b<a);
+		   System.out.println(a>b&&b<a);
+		   System.out.println(a<b||b<a);
+		   System.out.println(a>b||b<a);
+		   
+			System.out.println(a++ + ++a);
+			System.out.println(b-- + --b);
+
+
+	}
+
+}
